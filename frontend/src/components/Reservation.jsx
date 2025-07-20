07236acbc281ae679a92509fbb3acc23dc236734
Abhishek Raj <ar148007@gmail.com>
2025-07-20 18:20:44 +0530

    commit

--- frontend/src/components/Reservation.jsx
+++ frontend/src/components/Reservation.jsx
@@ -18,7 +18,7 @@ const Reservation = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "http://localhost:5000/api/v1/reservation/send",
+        "https://restaurant-reservation-deployed-1.onrender.com//api/v1/reservation/send",
         { firstName, lastName, email, phone, date, time },
         {
           headers: {
